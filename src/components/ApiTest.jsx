dc913638e640a52fcb1913d00bd8200afecbf52c
vaisrav <vaisrav@gmail.com>
2025-06-09 20:52:27 -0400

    use effect changes

--- src/components/ApiTest.jsx
+++ src/components/ApiTest.jsx
@@ -1,14 +1,14 @@
 import { useState, useEffect } from "react"
 
 export default function ApiTest(props) {
-    const [starWarsData, setstarWarsData] = useState(null)
-
-    useEffect(() => {
-        fetch("https://swapi.dev/api/people/1/")
-            .then(res => res.json())
-            .then(data => setstarWarsData(data))
-    }, [])
-
+    const {starWarsData, setstarWarsData} = useState(null)
+    
+    useEffect(function() {
+        fetch("https://swapi.tech/api/people/1/")
+        .then(res => res.json())
+        .then(data => setstarWarsData(data))
+    })
+    
     return (
         <div>
             <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
