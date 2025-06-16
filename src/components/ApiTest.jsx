58c52c7d132e65e58957de208f4fb768bc83deb1
vaisrav <vaisrav@gmail.com>
2025-06-16 19:05:14 -0400

    useEffect with dependancy array

--- src/components/ApiTest.jsx
+++ src/components/ApiTest.jsx
@@ -1,16 +1,24 @@
 import { useState, useEffect } from "react"
 
 export default function ApiTest(props) {
-    const {starWarsData, setstarWarsData} = useState(null)
+    const [starWarsData, setstarWarsData] = useState(null)
+    const [count, setCount] = useState(0)
     
-    useEffect(function() {
+    console.log("ApiTest component rendered")
+
+    useEffect(() => {
+        console.log("useEffect called")
         fetch("https://swapi.tech/api/people/1/")
         .then(res => res.json())
         .then(data => setstarWarsData(data))
-    })
+    }, [count]) 
     
     return (
         <div>
+            <h1>
+                The count is {count}
+            </h1>
+            <button onClick={() => setCount(prev => prev + 1)}>Add</button>
             <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
         </div>
     )
