ca5a22aa2566dd35387aa6c737e0a79ce1f90f27
vaisrav <vaisrav@gmail.com>
2025-06-17 20:08:48 -0400

    using useeffect to get memes form api

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 export default function Main() {
 
     const [meme, setMeme] = useState({
@@ -6,6 +6,14 @@ export default function Main() {
         bottomText: "Walk into Mordor",
         imageURL: "http://i.imgflip.com/1bij.jpg"
     })
+
+    const [allMemes, setAllMemes] = useState([])
+
+    useEffect(() => {
+        fetch("https://api.imgflip.com/get_memes")
+        .then(res => res.json())
+        .then(data => setAllMemes(data.data.memes))
+    }, [])
 
     function handleChange(event) { 
         const {value, name} = event.currentTarget
