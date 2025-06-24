166eb5a307a007429c0bc00595d923a1c431e586
vaisrav <vaisrav@gmail.com>
2025-06-24 19:00:04 -0400

    adding a clean up function to handle a bug

--- src/components/WindowTracker.jsx
+++ src/components/WindowTracker.jsx
@@ -4,9 +4,13 @@ export default function WindowTracker() {
     const [width, setWidth] = useState(window.innerWidth)
 
     useEffect(() => {
-        window.addEventListener("resize", function() {
+        function watchWindowWidth() {
             setWidth(window.innerWidth)
-        })
+        }
+        window.addEventListener("resize", watchWindowWidth)
+        return () => {
+            window.removeEventListener("resize", watchWindowWidth)
+        }
     }, [])
 
     return (
